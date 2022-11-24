15b9372387aa71075d38535181556ce49d95ff4c
neel3397 <neeldeda7@gmail.com>
2022-11-24 12:23:28 +0530

    d

---
+++ c programming/classwork/program3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("first name: neel\n");
+	printf("last name: deda\n");
+	printf("mobile no: 9265539153\n");
+	printf("birthdate: 3-3-1997\n");
+	
+}
---
+++ c programming/classwork/program5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	int n1=34,n2=65;
+	
+	printf("\naddition : %d",(n1+n2));
+	printf("\nsubtraction : %d",(n1-n2));
+	printf("\nmultiplication : %d",(n1*n2));
+	printf("\ndivision : %.3f",((float)n1/n2));
+	
+}
