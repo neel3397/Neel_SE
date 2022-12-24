f250aa82d0ab08fa45c0639072911d11670623b6
neel3397 <neeldeda7@gmail.com>
2022-12-24 11:29:23 +0530

    m

--- c programming/assignment/assignment 2 module 3/module3.3que3.c
+++ c programming/assignment/assignment 2 module 3/module3.3que3.c
@@ -1,22 +1,17 @@
 #include<stdio.h>
+void revstr()
+{
+	char c;
+	scanf("%c",&c);
+	if(c!='\n')
+	{
+		revstr();
+		printf("%c",c);
+	}
+}
+
 void main()
 {
-	char na[10];
-	printf("enter any string");
-	gets(na);
-	rev(na,0,strlen(na)-1);
-	printf("reverse of string %s",na);
-	
-	
-	getch();
-}
-void rev(char na[],int i,int I)
-{
-	char ch;
-	if(i>=1)
-	return;
-	ch=na[i];
-	na[i]=na[I];
-	na[I]=ch;
-	rev(na,++i,--i);
+	printf("enter a sentence:");
+	revstr();
 }
